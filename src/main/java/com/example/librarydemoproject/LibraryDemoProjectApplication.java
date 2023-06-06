9b13f6dc4f6b643687815a10265bba32b6d2a344
Umrbek004 <kuchkarovumrbek9@gmail.com>
2023-06-06 10:34:44 +0500

    firstCommit

---
+++ src/main/java/com/example/librarydemoproject/LibraryDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.librarydemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryDemoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryDemoProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/librarydemoproject/LibraryDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.librarydemoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryDemoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
